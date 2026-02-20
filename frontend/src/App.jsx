cc288a5061e8d420248fe2ba3f391600b916a6f1
LeeBellHa <jongha8273@naver.com>
2026-02-21 00:53:34 +0900

    ..

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Main from './pages/Main';
-import LoginPage from './pages/LoginPage';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Main from "./pages/Main";
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Routes>
-        {/* "/" 주소일 때는 Main 페이지를 보여줌 */}
         <Route path="/" element={<Main />} />
-        
-        {/* "/login" 주소일 때는 LoginPage 페이지를 보여줌 */}
-        <Route path="/login" element={<LoginPage />} />
+
+        {/* /login으로 들어와도 Main을 보여주고, Main에서 로그인 모달을 열게 함 */}
+        <Route path="/login" element={<Main />} />
+
+        {/* 없는 주소는 홈으로 */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
 }
-
-export default App;
